179f6bb19591426571a0dbaee768af3fa3dbd52b
Jenna Ransom <jenna.ransom@gmail.com>
2019-06-28 05:35:50 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,8 +2,8 @@ require 'pry'
 class Pokemon
   attr_accessor :name, :type, :db, :id, :hp
   
-  def initialize(id:, name:, type:, db:)
-    @id, @name, @type, @db = id, name, type, db
+  def initialize(id:, name:, type:, db:, hp: nil)
+    @id, @name, @type, @db, @hp = id, name, type, db, hp
   end 
   
   def self.save(name, type, db)
@@ -14,7 +14,7 @@ class Pokemon
     sql = "SELECT * FROM pokemon WHERE id = ?"
     
     pokemon = db.execute(sql, id).flatten
-    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db)
+    Pokemon.new(id: pokemon[0], name: pokemon[1], type: pokemon[2], db: db, hp: pokemon[3])
   end 
   
   def alter_hp(new_hp, db)
